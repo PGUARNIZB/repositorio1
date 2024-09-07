e2e2bd2272877eb9b20c7e3fea8a886e91eaa054
PAUL ALEXANDER <PAUL ALEXANDER@LAPTOP-V15UEK6J>
2024-09-07 09:06:41 -0500

    Cree el primer formulario

---
+++ src/repositorio1/Repositorio1.java
@@ -0,0 +1,17 @@
+
+package repositorio1;
+
+/**
+ *
+ * @author PAUL ALEXANDER
+ */
+public class Repositorio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
